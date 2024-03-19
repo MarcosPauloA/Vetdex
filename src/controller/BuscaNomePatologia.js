9ac04e2a5d0aa1a2adc5ef0594ae93eba1f128bb
Marcos Paulo Guida <mp1911@hotmail.com.br>
2024-03-18 23:02:20 -0300

    Fixed search bugs, added local search

--- src/controller/BuscaNomePatologia.js
+++ src/controller/BuscaNomePatologia.js
@@ -1,14 +1,21 @@
 import API_URL from "../model/config";
+import { fetchLocalPatologia } from "../model/saveLocalPatologia";
 
 export default async function BuscaNomePatologia(nomeBuscado){
     try {
         const resultado = await fetch(`${API_URL}/listaPatologias?nomePatologia=${nomeBuscado}`);
-        console.log("resultado " ,resultado)
-        data = await resultado.json();
-        console.log("data ", data)
-        return data;
+        if(JSON.stringify(resultado.status) == "200"){
+            data = await resultado.json();
+            return data;
+        } 
+        else {return []}
+
     }  catch (error){
-        console.log(error);
-        return {};
+        // Caso nao consiga puxar da api externa tenta puxar dos salvos locamente
+        objNome = {"nomePatologia": nomeBuscado};
+        const resultado = await fetchLocalPatologia(objNome);
+        if (resultado != undefined){
+            return [resultado];
+        } else {return []}
     }
 }
