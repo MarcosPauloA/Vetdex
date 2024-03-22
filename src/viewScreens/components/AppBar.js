358ef633bfd76bf1b18d895af058d8d2b7ff9d33
Marcos Paulo Guida <mp1911@hotmail.com.br>
2024-03-21 23:20:31 -0300

    fixed Modal issue

--- src/viewScreens/components/AppBar.js
+++ src/viewScreens/components/AppBar.js
@@ -2,16 +2,13 @@ import React, { useState } from "react";
 import { Modal, View, TextInput, StyleSheet, Button, Keyboard, Text, TouchableOpacity } from "react-native";
 // Biblioteca responsável pelo appbar
 import { AppBar, HStack, IconButton } from "@react-native-material/core";
-
 // Icones
 import Icon from "@expo/vector-icons/MaterialCommunityIcons";
 import { FontAwesome5, Feather } from '@expo/vector-icons';
-
 // Navegação
 import { useNavigation, useRoute } from '@react-navigation/native';
-import MostraNomesPatologias from "../../controller/MostraNomesPatologias";
-
-import { saveLocally } from "../../controller/MostraDetalhesPatologia";
+// Menu de opcoes quando clicado o icone de tres pontos
+import DropdownMenu from "./DropDownMenu";
 
 export default function appBar() {
     // O navigation servirá para retornar a página anterior quando clicado no ícone de voltar
@@ -30,18 +27,12 @@ export default function appBar() {
         const { nomePatologia } = route.params;
         titulo = nomePatologia.nomePatologia;
     }
-    const [modalVisible, setModalVisible] = useState(false)
+    const [searchBarVisible, setSearchBarVisible] = useState(false)
     const [fraseBusca, setFraseBusca] = useState("");
     const [optionsVisible, setOptionsVisible] = useState(false);
     return <>
         {/*Search Modal that is shown when the search icon is clicked*/}
-        <Modal
-            style={estilos.modal}
-            animationType="fade"
-            transparent={true}
-            visible={modalVisible}
-        >
-            <View style={estilos.container}>
+        { searchBarVisible && <View style={estilos.container}>
                 <View style={estilos.searchBarClicada}>
                     {/* search Icon */}
                     <Feather
@@ -58,47 +49,24 @@ export default function appBar() {
                         value={fraseBusca}
                         onChangeText={setFraseBusca}
                         onEndEditing={() => {
-                            setModalVisible(false)
+                            setSearchBarVisible(false)
                             navigation.navigate('Patologias', { nomeBuscado: { fraseBusca } })
                         }}
 
                     />
                 </View>
                 {/* cancel button, depending on whether the search bar is clicked or not */}
-                {modalVisible && (
+                {searchBarVisible && (
                     <Button
                         title="Cancelar"
                         onPress={() => {
                             Keyboard.dismiss();
-                            setModalVisible(false);
+                            setSearchBarVisible(false);
                         }}
                     ></Button>
                 )}
             </View>
-        </Modal>
-        
-        {/*Settings modal that is shown when the three dot icon is clicked*/}
-        <Modal
-            style={estilos.modal}
-            animationType="fade"
-            transparent={true}
-            visible={optionsVisible}
-        >   
-            <View style={estilos.container}>
-            <TouchableOpacity style={estilos.configButton}>
-                <Icon name="cog" size={24} color="#fff" />
-            </TouchableOpacity>
-            <Button style={estilos.configText} title="Salvar Offline" onPress={ () => {
-                if (route.name === "DetalhesDaPatologia"){
-                    saveLocally();
-                }
-            }}></Button>
-            <Button
-                        title="Cancelar"
-                        onPress={() => {setOptionsVisible(false);}}
-            ></Button>
-            </View>
-        </Modal>
+            }
 
         {/*Bellow is the default appbar and icons when nothing is clicked*/}
         <AppBar
@@ -111,18 +79,20 @@ export default function appBar() {
             trailing={props => (
                 <HStack>
                     <IconButton
-                        onPress={() => (setModalVisible(!modalVisible))}
+                        onPress={() => (setSearchBarVisible(!searchBarVisible))}
                         icon={props => <Icon name="magnify" {...props} />}
                         {...props}
                     />
                     <IconButton
-                        onPress={() => setOptionsVisible(!optionsVisible)}
+                        onPress={() => (setOptionsVisible(!optionsVisible))}
                         icon={props => <Icon name="dots-vertical" {...props} />}
                         {...props}
                     />
                 </HStack>
             )}
         />
+        {/*Settings modal that is shown when the three dot icon is clicked*/}
+        {optionsVisible && <DropdownMenu/>}
     </>
 }
 
